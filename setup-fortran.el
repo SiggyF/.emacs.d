53fa7b18b92d64e9919430dc70507c017e971cf2
Fedor Baart <fedor.baart@deltares.nl>
2013-02-01 01:04:53 +0100

    Fix issue with removing ampersand on save

--- setup-fortran.el
+++ setup-fortran.el
@@ -4,6 +4,8 @@
 (add-hook 'f90-mode-hook
           (lambda ()
             (fci-mode 1)
+	    (setq f90-mode t)
+            (setq fci-column 70)
             (whitespace-mode 1)
             )
           )
