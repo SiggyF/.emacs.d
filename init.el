ef80dc715961f975b0f1ce28e30feec850bafd2b
Fedor Baart <fedor.baart@deltares.nl>
2013-09-08 10:22:30 +0200

    no longer diminish

--- init.el
+++ init.el
@@ -64,9 +64,6 @@
      gist
      htmlize
      visual-regexp
-     ido-vertical-mode
-     simple-httpd
-     slime-js
      flycheck
      flx
      flx-ido
@@ -242,9 +239,6 @@
 (require 'my-misc)
 (when is-mac (require 'mac))
 
-;; Diminish stuff
-(require 'diminish)
-
 ;; Elisp go-to-definition with M-. and back again with M-,
 (autoload 'elisp-slime-nav-mode "elisp-slime-nav")
 (add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t) (eldoc-mode 1)))
@@ -261,9 +255,6 @@
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
-
-;; Diminish modeline clutter
-(diminish 'yas/minor-mode)
 
 ;; Conclude init by setting up specifics for the current user
 (when (file-exists-p user-settings-dir)
