e1d7a729ecd95a271b29e50bc0b1725f887ed95a
Fedor Baart <fedor.baart@deltares.nl>
2013-09-12 09:40:52 +0200

    setup god in setup file

---
+++ setup-god.el
@@ -0,0 +1,37 @@
+;;; setup-god.el --- Setup the god mode
+
+;; Copyright (C) 2013  Fedor Baart
+
+;; Author: Fedor Baart <fedorbaart@Fedor-Baarts-MacBook-Pro.local>
+;; Keywords: local, internal
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+(require 'god-mode)
+(global-set-key (kbd "<escape>") 'god-local-mode)
+(defun my-update-cursor ()
+  (setq cursor-type (if (or god-local-mode buffer-read-only)
+                        'box
+                      'bar)))
+
+(add-hook 'god-mode-enabled-hook 'my-update-cursor)
+(add-hook 'god-mode-disabled-hook 'my-update-cursor)
+
+(provide 'setup-god)
+;;; testing.el ends here
