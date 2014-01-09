8171f1b3f98a69f56eb42097c738c69cf40c6e11
Fedor Baart <fedor.baart@deltares.nl>
2014-01-09 21:47:36 +0100

    unique

--- defuns/editing-defuns.el
+++ defuns/editing-defuns.el
@@ -319,3 +319,17 @@ region-end is used."
   (while (not (looking-at "}"))
     (join-line -1))
   (back-to-indentation))
+
+
+(defun uniquify-region-lines (beg end)
+  "Remove duplicate adjacent lines in region."
+  (interactive "*r")
+  (save-excursion
+    (goto-char beg)
+    (while (re-search-forward "^\\(.*\n\\)\\1+" end t)
+      (replace-match "\\1"))))
+
+(defun uniquify-buffer-lines ()
+  "Remove duplicate adjacent lines in the current buffer."
+  (interactive)
+  (uniquify-region-lines (point-min) (point-max)))
