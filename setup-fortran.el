2497d5f055967c228d2e0633d97797366c4ec721
Fedor Baart <fedor.baart@deltares.nl>
2014-11-28 00:11:04 +0100

    flymake fortran

--- setup-fortran.el
+++ setup-fortran.el
@@ -6,6 +6,8 @@
             (setq f90-mode t)
             (setq fci-column 70)
             (whitespace-mode 1)
+            (flycheck-mode 1)
+            (setq flycheck-gfortran-language-standard "f2008")
             )
           )
 (provide 'setup-fortran)
